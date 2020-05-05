022fe443828eca45e1b0948a9472c584d71aec56
rigel selmani <rigelrigelselmani@hotmail.com>
2020-05-05 15:35:02 -0400

    apiKey

--- weather.js
+++ weather.js
@@ -1,5 +1,15 @@
+var APIKey="0ab9af27ca97b79fdc2b37ec61800370&units=imperial"
 
-getDate()
+var queryURL="http://api.openweathermap.org/data/2.5/weather?q=ashburn,virginia&appid="+APIKey
+
+$.ajax({
+    url:queryURL,
+    method:"GET"
+}).then(function(response){
+   console.log(response.main.temp)
+   $(".state").text(response.name)
+   $(".temp").text(Math.floor(response.main.temp+"o"))
+});
 
 function getDate(){
 
@@ -12,3 +22,5 @@ function getDate(){
     let today  = new Date();
     $("#date").text(today.toLocaleDateString("en-US", options));
     }
+
+getDate()
