a725832c21e180b1d2724f83cc16ae7339c4ec6a
rigel selmani <rigelrigelselmani@hotmail.com>
2020-05-07 01:52:34 -0400

    current location api

--- weather.js
+++ weather.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
 
 function addLocation(){
-    var city=$("input[type='text']").val().trim();
-    var queryURL="http://api.openweathermap.org/data/2.5/weather?q="
-    var apiKey="&appid=0ab9af27ca97b79fdc2b37ec61800370&units=imperial"
+    // var city=$("input[type='text']").val().trim();
+
+    if(navigator.geolocation){
+        console.log("geolocation aviable")
+        navigator.geolocation.getCurrentPosition(position =>{
+            var latit=position.coords.latitude;
+            console.log(latit)
+            var longi=position.coords.longitude;
+            console.log(longi)
+          var queryURL="http://api.openweathermap.org/data/2.5/weather?lat="+latit+"&lon="+longi+"&appid=0ab9af27ca97b79fdc2b37ec61800370&units=imperial"
    
     $.ajax({
-        url:queryURL+city+apiKey,
+        url:queryURL,
         method:"GET"
     }).then(function(response){
         console.log(response)
@@ -15,7 +22,12 @@ function addLocation(){
        $(".wind").text("Wind speed: "+Math.floor(response.wind.speed))
        $(".humidity").text("Humidity: "+response.main.humidity+"%")
      });
+    });
+}else{
+    console.log("Geolocation is not suported");
 }
+}
+addLocation()
 $("input").keypress(function(event){
     if(event.which===13){
         addLocation()
@@ -23,17 +35,6 @@ $("input").keypress(function(event){
 })
 
 $("button").on("click",function(){
-
-    if(navigator.geolocation)
-        navigator.geolocation.getCurrentPosition(function(position){
-            console.log(position);
-            $.get( "http://maps.googleapis.com/maps/api/geocode/json?latlng="+position.coords.latitude+","+position.coords.longitude+"&sensor-false",function(data){
-                console.log(data);
-            });
-        });
-    else
-        console.log("Geolocation is not suported");
-    
 });
 function getDate(){
 
