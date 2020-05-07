64dd19be4370fc5ce92b56f500759ebafd188b1e
rigel selmani <rigelrigelselmani@hotmail.com>
2020-05-07 04:19:48 -0400

    styling container1

--- weather.js
+++ weather.js
@@ -1,41 +1,44 @@
 $(document).ready(function(){
 
 function addLocation(){
-    // var city=$("input[type='text']").val().trim();
-
     if(navigator.geolocation){
-        console.log("geolocation aviable")
-        navigator.geolocation.getCurrentPosition(position =>{
-            var latit=position.coords.latitude;
-            console.log(latit)
-            var longi=position.coords.longitude;
-            console.log(longi)
-          var queryURL="http://api.openweathermap.org/data/2.5/weather?lat="+latit+"&lon="+longi+"&appid=0ab9af27ca97b79fdc2b37ec61800370&units=imperial"
-   
+             console.log("geolocation aviable")
+             navigator.geolocation.getCurrentPosition(position =>{
+                 var latit=position.coords.latitude;
+                 console.log(latit)
+                 var longi=position.coords.longitude;
+                 console.log(longi)
+               var queryURL="http://api.openweathermap.org/data/2.5/weather?lat="+latit+"&lon="+longi+"&appid=0ab9af27ca97b79fdc2b37ec61800370&units=imperial"
+        
+            $.ajax({
+               url:queryURL,
+               method:"GET"
+            }).then(function(response){
+               console.log(response)
+              $(".state").text(response.name)
+              $(".temp").text(Math.floor(response.main.temp)+" °")
+              $(".wind").text("Wind: "+Math.floor(response.wind.speed)+" MPH")
+              $(".humidity").text("Humidity: "+response.main.humidity+" %")
+            });
+        });
+    }else{
+        console.log("Geolocation is not suported");
+    }
+}
+
+function fiveDaysWeather(){
+    var queryURL="http://api.openweathermap.org/data/2.5/forecast?lat=39.0344729&lon=-77.52617599999999&appid=0ab9af27ca97b79fdc2b37ec61800370&units=imperial";
+
     $.ajax({
         url:queryURL,
         method:"GET"
     }).then(function(response){
         console.log(response)
-       $(".state").text(response.name)
-       $(".temp").text("temp: "+Math.floor(response.main.temp)+"°")
-       $(".wind").text("Wind speed: "+Math.floor(response.wind.speed))
-       $(".humidity").text("Humidity: "+response.main.humidity+"%")
-     });
-    });
-}else{
-    console.log("Geolocation is not suported");
-}
+    })
 }
 addLocation()
-$("input").keypress(function(event){
-    if(event.which===13){
-        addLocation()
-    }
-})
+fiveDaysWeather()
 
-$("button").on("click",function(){
-});
 function getDate(){
 
     let options = {
