6b9a19d39aa43e018f30e371a7cf70a46c8c521a
rigel selmani <rigelrigelselmani@hotmail.com>
2020-05-11 17:06:34 -0400

    orginizing js code

--- weather.js
+++ weather.js
@@ -6,9 +6,7 @@ $(document).ready(function(){
              console.log("geolocation aviable")
              navigator.geolocation.getCurrentPosition(position =>{
                  var latit=position.coords.latitude;
-                 console.log(latit)
                  var longi=position.coords.longitude;
-                 console.log(longi)
 
             var queryURL="https://api.openweathermap.org/data/2.5/weather?lat="+latit+"&lon="+longi+"&appid=0ab9af27ca97b79fdc2b37ec61800370&units=imperial";
         
@@ -17,28 +15,10 @@ $(document).ready(function(){
                method:"GET"
             }).then(function(response){
 
-              $(".state").text(response.name)
-              $(".temp").text(Math.floor(response.main.temp)+" °")
-              $(".wind").text("Wind: "+Math.floor(response.wind.speed)+" MPH")
-              $(".humidity").text("Humidity: "+response.main.humidity+" %")
-              $(".cloudy").text(response.weather[0].main)
+                apiResponse(response)
+                tempConverter(response)
+                imageDisplay(response)
 
-              let celcius=(response.main.temp -32) *(5/9);
-              $(".c").on("click",function(){
-                $(".temp").text(Math.floor(celcius)+" °")
-              })
-              console.log(Math.floor(celcius))
-              $(".f").on("click",function(){
-                  $(".temp").text(Math.floor(response.main.temp)+" °")
-              })
-
-              if(response.weather[0].main=="Rain"){
-                $('.container1').css('background-image', 'url(https://cdn.vox-cdn.com/thumbor/LF-o7juzy4AMUa2p6i8qWaFw4xU=/0x0:2048x1156/1200x800/filters:focal(299x94:625x420)/cdn.vox-cdn.com/uploads/chorus_image/image/49587899/15746767658_8338d05a3e_k.0.jpg)');              
-            }else if(response.weather[0].main=="Clouds"){
-                $('.container1').css('background-image', 'url(https://www.farmersalmanac.com/wp-content/uploads/2011/09/Clouds-Predict-Local-Weather-i861387936.jpg)');              
-            }else if(response.weather[0].main=="Clear"){
-                $('.container1').css('background-image', 'url(https://get.pxhere.com/photo/cloud-sky-white-weather-cumulus-blue-toy-sunny-clouds-clouded-sky-clouds-form-sunny-day-cumulus-clouds-cumulus-cloud-summer-day-meteorological-phenomenon-atmosphere-of-earth-1287206.jpg)');              
-            }
             });
         });
     }else{
@@ -56,39 +36,42 @@ $("input[type='text']").keypress(function(event){
             url:queryURL,
             method:"GET"
         }).then(function(response){
-
-          $(".temp").text(Math.floor(response.main.temp)+" °");
-          $(".state").text(response.name);
-          $(".humidity").text("Humidity: "+Math.floor(response.main.humidity)+" %");
-          $(".wind").text("Wind Speed: "+Math.floor(response.wind.speed));
-          $(".cloudy").text(response.weather[0].main)
-
-          let celcius=(response.main.temp -32) *(5/9);
-          $(".c").on("click",function(){
-            $(".temp").text(Math.floor(celcius)+" °")
-          })
-          console.log(Math.floor(celcius))
-          $(".f").on("click",function(){
-              $(".temp").text(Math.floor(response.main.temp)+" °")
-          })
-
-        //   var time=response.sys.sunrise
-        //   var date = new Date(time * 1000);
-        //   var timeStr = date.toLocaleTimeString();
         
-
-          if(response.weather[0].main==="Rain"){
-            $('.container1').css('background-image', 'url(http://1.bp.blogspot.com/-zLSkuSN9qBY/TiYKIIAl0eI/AAAAAAAAAOg/XKb3fo6LOhs/s1600/paris-rain-1920x1200.jpg)');              
-        }else if(response.weather[0].main==="Clouds"){
-            $('.container1').css('background-image', 'url(https://www.farmersalmanac.com/wp-content/uploads/2011/09/Clouds-Predict-Local-Weather-i861387936.jpg)');              
-        }else if(response.weather[0].main==="Clear"){
-            $('.container1').css('background-image', 'url(https://get.pxhere.com/photo/cloud-sky-white-weather-cumulus-blue-toy-sunny-clouds-clouded-sky-clouds-form-sunny-day-cumulus-clouds-cumulus-cloud-summer-day-meteorological-phenomenon-atmosphere-of-earth-1287206.jpg)');              
-        }
+           apiResponse(response)
+           tempConverter(response)
+           imageDisplay(response)
         })
     }
-});
-        
- 
+});   
+
+    function apiResponse(response){
+        $(".temp").text(Math.floor(response.main.temp)+" °")
+        $(".state").text(response.name);
+        $(".wind").text("Wind: "+Math.floor(response.wind.speed)+" MPH")
+        $(".humidity").text("Humidity: "+response.main.humidity+" %")
+        $(".cloudy").text(response.weather[0].main)
+    }
+    
+    function imageDisplay(response){
+        if(response.weather[0].main=="Rain"){
+            $('.container').css('background-image', 'url(https://cdn.abcotvs.com/dip/images/5184599_031119-kgo-shutterstock-rain-img.jpg?w=1600)');              
+        }else if(response.weather[0].main=="Clouds"){
+            $('.container').css('background-image', 'url(https://www.farmersalmanac.com/wp-content/uploads/2011/09/Clouds-Predict-Local-Weather-i861387936.jpg)');              
+        }else if(response.weather[0].main=="Clear"){
+            $('.container').css('background-image', 'url(https://theliberal.ie/wp-content/uploads/2016/08/sunnyspelll.jpg)');              
+        }
+    }
+
+    function tempConverter(response){
+        let celcius=(response.main.temp -32) *(5/9);
+        $(".c").on("click",function(){
+          $(".temp").text(Math.floor(celcius)+" °")
+        })
+        $(".f").on("click",function(){
+            $(".temp").text(Math.floor(response.main.temp)+" °")
+        })
+    }
+
 
     function getDate(){
 
@@ -104,31 +87,4 @@ $("input[type='text']").keypress(function(event){
     }
 
 addLocation()
-// fiveDaysWeather()
 });
-
-// function fiveDaysWeather(){
-
-//     if(navigator.geolocation){
-//         console.log("geolocation aviable")
-//         navigator.geolocation.getCurrentPosition(position =>{
-//             var latit=position.coords.latitude;
-//             console.log(latit)
-//             var longi=position.coords.longitude;
-//             console.log(longi)
-//     var queryURL="https://api.openweathermap.org/data/2.5/forecast?lat="+latit+"&lon="+longi+"&appid=0ab9af27ca97b79fdc2b37ec61800370&units=imperial";
-
-//     $.ajax({
-//         url:queryURL,
-//         method:"GET"
-//     }).then(function(response){
-//         var p1=$("<p>");
-//           p1.addClass("lTemperature");
-//           p1.text(Math.floor(response.list[2].main.temp_min)+"-"+Math.floor(response.list[2].main.temp_max))
-//           $(".lists").prepend(p1);   
-//        })
-//     })
-//   }else{
-//       console.log("geolocation not suported")
-//   }
-// }
