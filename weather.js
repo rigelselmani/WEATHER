7ece9f3a6f2fd98cfe74c79b480a0fd9ca232946
rigel selmani <rigelrigelselmani@hotmail.com>
2020-06-05 22:12:13 -0400

    changes

--- weather.js
+++ weather.js
@@ -7,8 +7,8 @@ $(document).ready(function(){
              navigator.geolocation.getCurrentPosition(position =>{
                  var latit=position.coords.latitude;
                  var longi=position.coords.longitude;
-            console.log(latit);
-            console.log(longi);
+            // console.log(latit);
+            // console.log(longi);
             var queryURL="https://api.openweathermap.org/data/2.5/weather?lat="+latit+"&lon="+longi+"&appid=0ab9af27ca97b79fdc2b37ec61800370&units=imperial";
         
             $.ajax({
@@ -44,7 +44,7 @@ $("input[type='text']").keypress(function(event){
         });
     }
 });   
-
+   //daily weather   for next 5 days
 //   function daily(){
 
 //     if(navigator.geolocation){
@@ -52,8 +52,7 @@ $("input[type='text']").keypress(function(event){
 //         navigator.geolocation.getCurrentPosition(position =>{
 //             var latit=position.coords.latitude;
 //             var longi=position.coords.longitude;
-//        console.log(latit);
-//        console.log(longi);
+   
 //     const queryURL="https://api.openweathermap.org/data/2.5/onecall?lat="+latit+"&lon="+longi+"&exclude=dayli&appid=0ab9af27ca97b79fdc2b37ec61800370&units=imperial"
 //     $.ajax({
 //         url:queryURL,
@@ -70,12 +69,16 @@ $("input[type='text']").keypress(function(event){
 // }   
 
 //     function firstDay(respons){
-//         console.log(respons.daily)
+        // console.log(respons.daily)
+        // navigator.geolocation.getCurrentPosition(function(position) {
+        //     var sunset = new Date().sunset(position.coords.latitude, position.coords.longitude)
+        //     console.log(sunset);
+    //  });
 //         $(".day-min1").text(Math.floor(respons.daily[0].temp.min)+" °")
 //         $(".day-max1").text(Math.floor(respons.daily[0].temp.max)+" °")
 //     }
 // daily()
-
+    // end of daily logic for next 5 days
     function apiResponse(response){
         $(".temp").text(Math.floor(response.main.temp)+" °")
         $(".state").text(response.name);
@@ -85,10 +88,11 @@ $("input[type='text']").keypress(function(event){
     }
     
     function imageDisplay(response){
+     
         if(response.weather[0].main=="Rain"){
             $('.container1').css('background-image', 'url(http://www.nosmokeandmirrors.com/wp-content/uploads/2016/12/drops-1436231_1280.jpg)');              
         }else if(response.weather[0].main=="Clouds"){
-            $('.container1').css('background-image', 'url(https://sunmodo.com/wp-content/uploads/2015/08/solar-on-cloudy-days.jpg)');              
+            $('.container1').css('background-image', 'url(https://eoimages.gsfc.nasa.gov/images/imagerecords/84000/84662/rover__pho_7268_lrg.jpg)');              
         }else if(response.weather[0].main=="Clear"){
             $('.container1').css('background-image', 'url(https://theliberal.ie/wp-content/uploads/2016/08/sunnyspelll.jpg)');              
         }
